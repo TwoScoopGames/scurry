5de8847f751fe57fa11266c688e502f1e11f7b03
Eric Lathrop <eric@ericlathrop.com>
2014-01-22 21:25:31 -0500

    Move Game class to separate file.

--- splat/scene.js
+++ splat/scene.js
@@ -71,31 +71,8 @@ var Splat = (function(splat, window, document) {
 		};
 	}
 
-	function loadAssets(assetLoader, assets) {
-		for (var key in assets) {
-			if (assets.hasOwnProperty(key)) {
-				assetLoader.load(key, assets[key]);
-			}
-		}
-	}
-	function Game(canvas, manifest) {
-		this.mouse = new splat.MouseInput(canvas);
-		this.keyboard = new splat.KeyboardInput(splat.keyMap.US);
-
-		this.images = new splat.ImageLoader();
-		loadAssets(this.images, manifest.images);
-		this.sounds = new splat.SoundLoader();
-		loadAssets(this.sounds, manifest.sounds);
-
-		var that = this;
-		this.isLoaded = function() {
-			return this.images.allLoaded() && this.sounds.allLoaded();
-		};
-	}
-
 	splat.time = time;
 	splat.Scene = Scene;
-	splat.Game = Game;
 	return splat;
 
 }(Splat || {}, window, document));
